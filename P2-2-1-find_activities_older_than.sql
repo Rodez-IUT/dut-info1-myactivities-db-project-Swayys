4fea5bc12a0a26a107fbec879660143dd18f2558
RemiS <r.saurel5@gmail.com>
2020-04-01 09:31:14 +0200

    fix #4 fonction P2-2-1 fonction find_activities_older_than

--- P2-2-1-find_activities_older_than.sql
+++ P2-2-1-find_activities_older_than.sql
@@ -1,10 +1,12 @@
 Description de la fonction :  
 
     - *Nom de la fonction* :  
-    CREATE FUNCTION find_activities_older_than(old_date) RETURNS activity AS $$
+    CREATE FUNCTION find_activities_older_than(modification_date date, old_date date) RETURNS activity AS $$
         UPDATE activity
-        WHERE modification_date < old_date;
-        RETURNING activity
+		SET modification_date = find_activities_older_than.modification_date
+		WHERE modification_date < old_date
+        RETURNING activity;
+    $$ LANGUAGE SQL;
 
     - *Description* :   
     la fonction retourne la liste des activités qui n'ont pas été modifiées depuis la date "old_date" passée en paramètre.
